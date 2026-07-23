6cfe05a47191fbc01a1d6a7e4307f5b25ac4a371
name <akanuragkumar1112202@gmail.com>
2026-07-23 23:43:15 +0530

    adding solution to ques no 172

---
+++ Advance Data Structure Lab/Leetcode/172FactorialTrailingZeroes.cpp
@@ -0,0 +1,14 @@
+#include <bits\stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int trailingZeroes(int n) {
+        long long num = n;
+        long long fResult = tgamma(n + 1);
+    }
+};
+
+int main(){
+
+}
