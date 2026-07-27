22d2b54e5d5e5f701ddc3ecabde0988b47826825
name <akanuragkumar1112202@gmail.com>
2026-07-27 14:20:46 +0530

    adding solution to ques no. 1464

---
+++ Advance Data Structure Lab/Leetcode/1464MaximumProductofTwoElementsinanArray.cpp
@@ -0,0 +1,17 @@
+#include <bits\stdc++.h>
+using namespace std;
+
+
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int s = nums.size();
+        return((nums[s-1]-1)*(nums[s-2]-1));
+    }
+};
+
+
+int main(){
+
+}
