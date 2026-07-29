938e65d46d91c8194688c92d01fede4b5214da7d
name <akanuragkumar1112202@gmail.com>
2026-07-29 23:39:42 +0530

    adding dp solution version of ques no 70

--- Advance Data Structure Lab/Leetcode/70ClimbingStairs.cpp
+++ Advance Data Structure Lab/Leetcode/70ClimbingStairs.cpp
@@ -18,6 +18,30 @@ public:
         return b;
     }
 };
+
+
+//using DP 
+class Solution1 {
+public:
+    int climbStairs(int n) {
+        if (n <= 2) {
+            return n;
+        }
+        
+        vector<int> dp(n + 1);
+        
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        return dp[n];
+    }
+};
+
+
 int main(){
     int n;
     cin>>n;
