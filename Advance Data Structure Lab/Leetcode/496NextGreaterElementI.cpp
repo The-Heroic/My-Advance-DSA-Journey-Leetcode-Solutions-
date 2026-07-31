6b3e090fef793f3c4643a6e62c511f1653eaf199
name <akanuragkumar1112202@gmail.com>
2026-07-31 22:54:20 +0530

    adding solution to ques no. 496

---
+++ Advance Data Structure Lab/Leetcode/496NextGreaterElementI.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        vector<int> Nge;
+        
+        for(int i =0; i< nums2.size(); i++){
+            int j = 0;
+            int num = nums1[j];
+            
+        }
+    }
+};
+
+int main(){
+
+}
