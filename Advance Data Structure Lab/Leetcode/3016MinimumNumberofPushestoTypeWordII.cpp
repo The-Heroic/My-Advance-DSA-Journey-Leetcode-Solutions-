119b4ce76827997e9a97f14df265d4542ebf6685
name <akanuragkumar1112202@gmail.com>
2026-08-16 23:32:55 +0530

    adding solution for ques no 3016

---
+++ Advance Data Structure Lab/Leetcode/3016MinimumNumberofPushestoTypeWordII.cpp
@@ -0,0 +1,29 @@
+#include <bits\stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int minimumPushes(string word) {
+        vector<int> freq(26, 0);
+
+        for (char c : word) {
+            freq[c - 'a']++;
+        }
+
+        sort(freq.rbegin(), freq.rend());
+
+        int totalPushes = 0;
+        for (int i = 0; i < 26; i++) {
+            if (freq[i] == 0) break; 
+
+            int pushesPerChar = (i / 8) + 1;
+            totalPushes += freq[i] * pushesPerChar;
+        }
+
+        return totalPushes;
+    }
+};
+
+int main(){
+
+}
