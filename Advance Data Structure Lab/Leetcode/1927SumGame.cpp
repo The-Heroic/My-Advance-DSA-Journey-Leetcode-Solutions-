f0eca1200819b4e9d62f77186303313c74a1e466
name <akanuragkumar1112202@gmail.com>
2026-08-24 23:45:27 +0530

    adding solution of ques no 1927(modified)

--- Advance Data Structure Lab/Leetcode/1927SumGame.cpp
+++ Advance Data Structure Lab/Leetcode/1927SumGame.cpp
@@ -4,8 +4,46 @@ using namespace std;
 class Solution {
 public:
     bool sumGame(string num) {
-        
-        
+        int n = num.size();
+        int mid = n / 2;
+
+        int leftSum = 0;
+        int rightSum = 0;
+        int leftQ = 0;
+        int rightQ = 0;
+
+        for (int i = 0; i < mid; i++) {
+            if (num[i] == '?') {
+                leftQ++;
+            } else {
+                leftSum += (num[i] - '0');
+            }
+        }
+
+        for (int i = mid; i < n; i++) {
+            if (num[i] == '?') {
+                rightQ++;
+            } else {
+                rightSum += (num[i] - '0');
+            }
+        }
+
+        if (leftQ == 0 && rightQ == 0) {
+            return leftSum != rightSum; 
+        }
+
+            if ((leftQ + rightQ) % 2 != 0) {
+            return true;
+        }
+
+        int diffSum = leftSum - rightSum;
+        int diffQ = rightQ - leftQ;
+
+        if (2 * diffSum == 9 * diffQ) {
+            return false; 
+        }
+
+        return true;
     }
 };
 
