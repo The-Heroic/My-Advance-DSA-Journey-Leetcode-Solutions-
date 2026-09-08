b777033007dc7cb27180b0915a73f880aba05a38
name <akanuragkumar1112202@gmail.com>
2026-09-08 23:41:50 +0530

    adding solution of ques no 3870

---
+++ Advance Data Structure Lab/Leetcode/3870CountCommasinRange.cpp
@@ -0,0 +1,20 @@
+#include <bits\stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int countCommas(int n) {
+        int totalCommas = 0;
+
+        for (int currentVal = 1; currentVal <= n; currentVal++) {
+            string strVal = to_string(currentVal);
+            totalCommas += (strVal.length() - 1) / 3;
+        }
+
+        return totalCommas;
+    }
+};
+
+int main(){
+    
+}
