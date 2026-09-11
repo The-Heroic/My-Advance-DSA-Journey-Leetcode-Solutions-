480fa991b472339abd8d8cd953416b9111511abd
name <akanuragkumar1112202@gmail.com>
2026-09-12 02:48:21 +0530

    modifying solution of ques no 3483

--- Advance Data Structure Lab/Leetcode/3483Unique3-DigitEvenNumbers.cpp
+++ Advance Data Structure Lab/Leetcode/3483Unique3-DigitEvenNumbers.cpp
@@ -4,10 +4,23 @@ using namespace std;
 class Solution {
 public:
     int totalNumbers(vector<int>& digits) {
-        
+        int n = digits.size();
+        unordered_set<int> numSet;
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                for (int k = 0; k < n; ++k) {
+                    if (i != j && j != k && i != k) {
+                        if (digits[i] != 0 && digits[k] % 2 == 0) {
+                            int num = digits[i] * 100 + digits[j] * 10 + digits[k];
+                            numSet.insert(num);
+                        }
+                    }
+                }
+            }
+        }
+    return numSet.size();
     }
 };
-
 int main(){
 
 }
