893eb8c547498254e280fc44e816f43f3d84de30
name <akanuragkumar1112202@gmail.com>
2026-09-15 22:44:24 +0530

    adding solution of ques no 2441

---
+++ Advance Data Structure Lab/Leetcode/2441LargestPositiveIntegerThatExistsWithItsNegative.cpp
@@ -0,0 +1,30 @@
+#include <bits\stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int findMaxK(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+
+        int left = 0;
+        int right = nums.size() - 1;
+
+        while (left < right) {
+            int sum = nums[left] + nums[right];
+
+            if (sum == 0) {
+                return nums[right];
+            } else if (sum < 0) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return -1;
+    }
+};
+
+int main(){
+
+}
